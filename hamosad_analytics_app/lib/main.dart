1c33e984068e016205f603fad6fbb28f680c25c9
Sufilevy <sufilevy@gmail.com>
2022-11-04 18:41:50 +0200

    Moved analytics project files into correct folder

--- hamosad_analytics_app/lib/main.dart
+++ hamosad_analytics_app/lib/main.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:hamosad_scouting_app_2/pages/team_details_page.dart';
+import 'components/nav_drawer.dart';
+import 'constants.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      title: 'Scouting Analytics',
+      theme: theme(),
+      home: TeamDetailsPage(),
+    );
   }
+
+  ThemeData theme() => ThemeData(
+    fontFamily: Consts.defaultFontFamily,
+    scaffoldBackgroundColor: Consts.backgroundColor,
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      backgroundColor: Consts.backgroundColor, 
+      titleTextStyle: TextStyle(
+        fontFamily: Consts.defaultFontFamily,
+        color: Consts.secondaryDisplayColor,
+        fontSize: 32,
+      )
+    ),
+  );
 }
