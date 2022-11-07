a066757805867a18ad60570eac310be447572dd5
liad-inon <52380897+liad-inon@users.noreply.github.com>
2022-11-07 10:36:28 +0200

    Fix MultiSelectionsButton.

--- hamosad_analytics_app/lib/components/multi_selections_button.dart
+++ hamosad_analytics_app/lib/components/multi_selections_button.dart
@@ -1,5 +1,6 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
+import 'package:hamosad_analytics_app/constants.dart';
 
 class MultiSelectionsButton extends StatelessWidget {
   const MultiSelectionsButton({
@@ -8,69 +9,68 @@ class MultiSelectionsButton extends StatelessWidget {
     required this.selectedItems,
     required this.button,
     required this.onChange,
-    required this.hint,
   });
 
   final List<String> items;
   final List<String> selectedItems;
   final Widget button;
   final Function(List<String>) onChange;
-  final Widget hint;
 
   @override
   Widget build(BuildContext context) {
     return DropdownButtonHideUnderline(
-        child: DropdownButton2(
-      isExpanded: true,
-      hint: Align(
-        alignment: AlignmentDirectional.center,
-        child: hint,
-      ),
-      items: items.map((item) {
-        return DropdownMenuItem<String>(
-          value: item,
-          //disable default onTap to avoid closing menu when selecting an item
-          enabled: false,
-          child: StatefulBuilder(
-            builder: (context, menuSetState) {
-              final _isSelected = selectedItems.contains(item);
-              return InkWell(
-                onTap: () {
-                  _isSelected
-                      ? selectedItems.remove(item)
-                      : selectedItems.add(item);
-                  onChange(selectedItems);
-                  //This rebuilds the dropdownMenu Widget to update the check mark
-                  menuSetState(() {});
-                },
-                child: Container(
-                  height: double.infinity,
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Row(
-                    children: [
-                      _isSelected
-                          ? const Icon(Icons.check_box_outlined)
-                          : const Icon(Icons.check_box_outline_blank),
-                      const SizedBox(width: 16),
-                      Text(
-                        item,
-                        style: const TextStyle(
-                          fontSize: 14,
+      child: DropdownButton2(
+        dropdownDecoration: const BoxDecoration(
+          color: Consts.sectionDefultColor,
+        ),
+        isExpanded: true,
+        items: items.map((item) {
+          return DropdownMenuItem<String>(
+            value: item,
+            enabled: false,
+            child: StatefulBuilder(
+              builder: (context, menuSetState) {
+                final isSelected = selectedItems.contains(item);
+                return InkWell(
+                  onTap: () {
+                    isSelected
+                        ? selectedItems.remove(item)
+                        : selectedItems.add(item);
+                    onChange(selectedItems);
+                    //This rebuilds the dropdownMenu Widget to update the check mark
+                    menuSetState(() {});
+                  },
+                  child: Container(
+                    height: double.infinity,
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Row(
+                      children: [
+                        isSelected
+                            ? const Icon(Icons.check_box_outlined)
+                            : const Icon(Icons.check_box_outline_blank),
+                        const SizedBox(width: 16),
+                        Text(
+                          item,
+                          style: const TextStyle(
+                            color: Consts.secondaryDisplayColor,
+                            fontSize: 14,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              );
-            },
-          ),
-        );
-      }).toList(),
-      //Use last selected item as the current value so if we've limited menu height, it scroll to last item.
-      value: selectedItems.isEmpty ? null : selectedItems.last,
-      onChanged: (value) {},
-      customButton: button,
-      itemPadding: EdgeInsets.zero,
-    ));
+                );
+              },
+            ),
+          );
+        }).toList(),
+        dropdownWidth: 200,
+        //Use last selected item as the current value so if we've limited menu height, it scroll to last item.
+        value: selectedItems.isEmpty ? null : selectedItems.last,
+        onChanged: (value) {},
+        customButton: button,
+        itemPadding: EdgeInsets.zero,
+      ),
+    );
   }
 }
