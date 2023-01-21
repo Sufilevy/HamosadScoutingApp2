76e1b3c1309081e57b492cdf73a77c9225ac0456
Sufilevy <sufilevy@gmail.com>
2023-01-21 12:38:37 +0200

    Moved AnalyticsFadeSwitcher to analytics.dart

--- hamosad_analytics_app/lib/src/widgets.dart
+++ hamosad_analytics_app/lib/src/widgets.dart
@@ -1,29 +1,5 @@
-import 'package:flutter/material.dart';
-
 export 'widgets/analytics.dart';
+export 'widgets/graphs.dart';
 export 'widgets/search_bar.dart';
 export 'widgets/sidebar.dart';
 export 'widgets/tab_selector.dart';
-
-class AnalyticsFadeSwitcher extends StatelessWidget {
-  const AnalyticsFadeSwitcher({
-    Key? key,
-    required this.child,
-  }) : super(key: key);
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSwitcher(
-      duration: const Duration(milliseconds: 250),
-      switchInCurve: Curves.easeInOut,
-      switchOutCurve: Curves.easeInOut,
-      transitionBuilder: (child, animation) => FadeTransition(
-        opacity: animation,
-        child: child,
-      ),
-      child: child,
-    );
-  }
-}
--- hamosad_analytics_app/lib/src/widgets/analytics.dart
+++ hamosad_analytics_app/lib/src/widgets/analytics.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:hamosad_analytics_app/src/constants.dart';
 
+class AnalyticsFadeSwitcher extends StatelessWidget {
+  const AnalyticsFadeSwitcher({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 250),
+      switchInCurve: Curves.easeInOut,
+      switchOutCurve: Curves.easeInOut,
+      transitionBuilder: (child, animation) => FadeTransition(
+        opacity: animation,
+        child: child,
+      ),
+      child: child,
+    );
+  }
+}
+
 class AnalyticsContainer extends StatelessWidget {
   const AnalyticsContainer({
     Key? key,
