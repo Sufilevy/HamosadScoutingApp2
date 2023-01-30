0433a83a870ef235d504b539e5a37f47689df83a
Sufilevy <sufilevy@gmail.com>
2023-01-31 00:43:35 +0200

    Added report models

--- hamosad_analytics_app/lib/src/models/report.dart
+++ hamosad_analytics_app/lib/src/models/report.dart
@@ -2,49 +2,34 @@ import 'package:hamosad_analytics_app/src/models.dart';
 import 'package:intl/intl.dart';
 
 class Report {
-  final ReportInfo info;
+  final int teamNumber;
+  final String match, scouter;
+  final DateTime time;
   final ReportAutonomus autonomus;
   final ReportTeleop teleop;
   final ReportEndgame endgame;
   final ReportSummary summary;
 
   Report({
-    required int teamNumber,
-    required String match,
-    required String scouter,
-    required DateTime time,
-  })  : info = ReportInfo(teamNumber, match, scouter, time),
-        autonomus = ReportAutonomus(),
+    required this.teamNumber,
+    required this.match,
+    required this.scouter,
+    required this.time,
+  })  : autonomus = ReportAutonomus(),
         teleop = ReportTeleop(),
         endgame = ReportEndgame(),
         summary = ReportSummary();
 
   Report.fromJson(Json json)
-      : info = ReportInfo.fromJson(json['info'] ?? {}),
+      : teamNumber = (json['info'] ?? {})['teamNumber'] ?? 0,
+        match = (json['info'] ?? {})['match'] ?? '',
+        scouter = (json['info'] ?? {})['scouter'] ?? '',
+        time = DateFormat('dd/MM HH:mm:ss')
+            .parse((json['info'] ?? {})['time'] ?? '01/01 00:00:00'),
         autonomus = ReportAutonomus.fromJson(json['autonomus'] ?? {}),
         teleop = ReportTeleop.fromJson(json['teleop'] ?? {}),
         endgame = ReportEndgame.fromJson(json['endgame'] ?? {}),
         summary = ReportSummary.fromJson(json['summary'] ?? {});
-}
-
-class ReportInfo {
-  final int teamNumber;
-  final String match, scouter;
-  final DateTime time;
-
-  const ReportInfo(
-    this.teamNumber,
-    this.match,
-    this.scouter,
-    this.time,
-  );
-
-  ReportInfo.fromJson(Json json)
-      : teamNumber = json['teamNumber'] ?? 0,
-        match = json['match'] ?? '',
-        scouter = json['scouter'] ?? '',
-        time = DateFormat('dd/MM HH:mm:ss')
-            .parse(json['time'] ?? '01/01 00:00:00');
 }
 
 class ReportAutonomus {
@@ -98,4 +83,171 @@ class ReportSummary {
         teleopScore = json['teleopScore'] ?? 0,
         endgameScore = json['endgameScore'] ?? 0,
         totalScore = json['totalScore'] ?? 0;
+}
+
+enum ActionDuration {
+  zeroToTwo,
+  twoToFive,
+  fivePlus;
+
+  @override
+  String toString() {
+    switch (this) {
+      case zeroToTwo:
+        return '0-2';
+      case twoToFive:
+        return '2-5';
+      case fivePlus:
+        return '5+';
+    }
+  }
+
+  static ActionDuration? fromString(String value) {
+    switch (value) {
+      case '0-2':
+        return zeroToTwo;
+      case '2-5':
+        return twoToFive;
+      case '5+':
+        return fivePlus;
+    }
+    return null;
+  }
+}
+
+enum GamePiecePickupPosition {
+  doubleLeftShelf,
+  doubleRightShelf,
+  doubleLeftFloor,
+  doubleRightFloor,
+  single,
+  floorStanding,
+  floorLaying;
+
+  static GamePiecePickupPosition? fromString(String value) {
+    switch (value) {
+      case 'doubleLeftShelf':
+        return doubleLeftShelf;
+      case 'doubleRightShelf':
+        return doubleRightShelf;
+      case 'doubleLeftFloor':
+        return doubleLeftFloor;
+      case 'doubleRightFloor':
+        return doubleRightFloor;
+      case 'single':
+        return single;
+      case 'floorStanding':
+        return floorStanding;
+      case 'floorLaying':
+        return floorLaying;
+    }
+    return null;
+  }
+}
+
+enum GamePiece {
+  cone,
+  cube;
+
+  static GamePiece? fromString(String value) {
+    switch (value) {
+      case 'cone':
+        return cone;
+      case 'cube':
+        return cube;
+    }
+    return null;
+  }
+}
+
+class GamePiecePickup {
+  const GamePiecePickup({
+    required this.duration,
+    required this.position,
+    required this.gamePiece,
+  });
+
+  final ActionDuration duration;
+  final GamePiecePickupPosition position;
+  final GamePiece gamePiece;
+
+  static GamePiecePickup? fromJson(Json json) {
+    final duration = ActionDuration.fromString(json['duration'] ?? '');
+    final position = GamePiecePickupPosition.fromString(json['position'] ?? '');
+    final gamePiece = GamePiece.fromString(json['gamePiece'] ?? '');
+
+    if (duration == null || position == null || gamePiece == null) {
+      return null;
+    }
+
+    return GamePiecePickup(
+      duration: duration,
+      position: position,
+      gamePiece: gamePiece,
+    );
+  }
+}
+
+class GamePieceDropoff {
+  const GamePieceDropoff({
+    required this.duration,
+    required this.row,
+    required this.column,
+    required this.gamePiece,
+  });
+
+  final ActionDuration duration;
+  final int row, column;
+  final GamePiece gamePiece;
+
+  static GamePieceDropoff? fromJson(Json json) {
+    final duration = ActionDuration.fromString(json['duration'] ?? '');
+    final row = json['row'];
+    final column = json['column'];
+    final gamePiece = GamePiece.fromString(json['gamePiece'] ?? '');
+
+    if (duration == null ||
+        row == null ||
+        column == null ||
+        gamePiece == null) {
+      return null;
+    }
+
+    return GamePieceDropoff(
+      duration: duration,
+      row: row,
+      column: column,
+      gamePiece: gamePiece,
+    );
+  }
+}
+
+enum CommunityPass {
+  left,
+  right;
+
+  static CommunityPass? fromString(String value) {
+    switch (value) {
+      case 'left':
+        return left;
+      case 'right':
+        return right;
+    }
+    return null;
+  }
+}
+
+enum LoadingZonePass {
+  start,
+  end;
+
+  static LoadingZonePass? fromString(String value) {
+    switch (value) {
+      case 'start':
+        return start;
+      case 'end':
+        return end;
+    }
+    return null;
+  }
 }
