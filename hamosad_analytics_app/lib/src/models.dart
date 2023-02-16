22be7fe657b2495f2014ce9ec4857dbebb1f83ac
Sufilevy <sufilevy@gmail.com>
2023-02-16 16:01:57 +0200

    Changed Stat class structure

--- hamosad_analytics_app/lib/src/models.dart
+++ hamosad_analytics_app/lib/src/models.dart
@@ -12,14 +12,28 @@ class Stat<T extends num> {
   final T min, max;
   final double average;
 
-  Stat({
+  const Stat({
     required this.min,
     required this.max,
     required this.average,
   });
 
-  static Stat<int> zero() => Stat(min: 0, max: 0, average: 0.0);
-  static Stat<double> zeroDoube() => Stat(min: 0.0, max: 0.0, average: 0.0);
-  static Stat<int> only({int? min, int? max, double? average}) =>
+  static Stat<int> zero() => const Stat(
+        min: 0,
+        max: 0,
+        average: 0.0,
+      );
+
+  static Stat<double> zeroDoube() => const Stat(
+        min: 0.0,
+        max: 0.0,
+        average: 0.0,
+      );
+
+  static Stat<int> only({
+    int? min,
+    int? max,
+    double? average,
+  }) =>
       Stat(min: min ?? 0, max: max ?? 0, average: average ?? 0.0);
 }
