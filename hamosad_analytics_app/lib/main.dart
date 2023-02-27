c533aebbdab1354e78e0fd8f3094ad71e7e00a84
Sufilevy <sufilevy@gmail.com>
2023-02-27 04:05:31 +0200

    Added preferred orientation

--- hamosad_analytics_app/lib/main.dart
+++ hamosad_analytics_app/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hamosad_analytics_app/src/app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+
   runApp(ProviderScope(child: AnalyticsApp()));
 }
