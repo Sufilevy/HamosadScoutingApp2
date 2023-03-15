727be12e990f8920c438db9c5066316dcff978f5
Sufilevy <sufilevy@gmail.com>
2023-03-16 00:08:27 +0200

    Started adding charts

--- hamosad_analytics_app/lib/src/widgets/graphs.dart
+++ hamosad_analytics_app/lib/src/widgets/graphs.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class CompareTeamsLineGraph extends StatefulWidget {
+  const CompareTeamsLineGraph({Key? key}) : super(key: key);
+
+  @override
+  State<CompareTeamsLineGraph> createState() => _CompareTeamsLineGraphState();
+}
+
+class _CompareTeamsLineGraphState extends State<CompareTeamsLineGraph> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
